e5c8e0e1a146144e6978dda5fc0d960c7fa5cc6a
Ryan Monson <ryandmonson@gmail.com>
2021-09-16 11:35:33 -0400

    update practice

--- Program.cs
+++ Program.cs
@@ -64,7 +64,38 @@ namespace MyPracticeCSharp
 
             Between(2, 8);
 
-            foreach (int i in myInt);
+            // Convert US Dollars into Yuan
+
+            static decimal ustoyuan (decimal dollar)
+            {
+                decimal yuan = 6.75m*dollar;
+                return yuan;
+
+            }
+
+            Console.WriteLine(ustoyuan(2));
+            /*
+            Timmy & Sarah think they are in love, but around where they live, 
+            they will only know once they pick a flower each. If one of the flowers 
+            has an even number of petals and the other has an odd number of petals 
+            it means they are in love.
+
+            Write a function that will take the number of petals of each flower and 
+            return true if they are in love and false if they aren't.
+            */
+
+           static bool lovefunc(int flower1, int flower2)
+            {
+                if ((flower1 % 2 != 0 && flower2 % 2 == 0) || (flower1 % 2 == 0 && flower2 % 2 != 0))
+                {
+                    return true;
+                } else
+                {
+                    return false;
+                }             
+            }
+            Console.WriteLine(lovefunc(2, 3));
+
         }
     }
 }
